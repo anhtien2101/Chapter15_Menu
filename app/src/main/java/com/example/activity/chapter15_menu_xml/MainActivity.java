9f78334a4098d71ae979aa695a5962f95dd3228a
Anh Tien <anhtien210@gmail.com>
2016-02-16 11:52:52 +0700

    demo menu by XML

---
+++ app/src/main/java/com/example/activity/chapter15_menu_xml/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.activity.chapter15_menu_xml;
+
+import android.app.ListActivity;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+
+public class MainActivity extends ListActivity {
+    private static final String[] items = { "lorem", "ipsum", "dolor", "sit",
+            "amet", "consectetuer", "adipiscing", "elit", "morbi", "vel",
+            "ligula", "vitae", "arcu", "aliquet", "mollis", "etiam", "vel",
+            "erat", "placerat", "ante", "porttitor", "sodales", "pellentesque",
+            "augue", "purus" };
+    public static final int MENU_ADD = Menu.FIRST + 1;
+    public static final int MENU_RESET = Menu.FIRST + 2;
+    public static final int MENU_CAP = Menu.FIRST + 3;
+    public static final int MENU_REMOVE = Menu.FIRST + 4;
+    ArrayList<String> words;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initAdapter();
+        registerForContextMenu(getListView());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        new MenuInflater(this).inflate(R.menu.option, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.menu_add:
+                add();
+                break;
+            case R.id.menu_reset:
+                initAdapter();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        new MenuInflater(this).inflate(R.menu.context, menu);
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        ArrayAdapter<String> adapter = (ArrayAdapter<String>) getListAdapter();
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.menu_cap:
+                String word = words.get(info.position);
+                word = word.toUpperCase();
+                adapter.remove(words.get(info.position));
+                adapter.insert(word, info.position);
+                break;
+            case R.id.menu_remove:
+                adapter.remove(words.get(info.position));
+                break;
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    private void initAdapter() {
+        words = new ArrayList<>();
+        for (String s : items) {
+            words.add(s);
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+        setListAdapter(adapter);
+    }
+
+    private void add(){
+        final View addView = getLayoutInflater().inflate(R.layout.add, null);
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this).setTitle("Add a word").setView(addView).
+                setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        ArrayAdapter<String> adapter = (ArrayAdapter<String>) getListAdapter();
+                        EditText edtWord = (EditText) addView.findViewById(R.id.edtWord);
+                        adapter.add(edtWord.getText().toString());
+                    }
+                });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+            }
+        });
+        builder.show();
+    }
+}
